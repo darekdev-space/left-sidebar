d2be9c5a9c116a65a42b867d3d2742fdb3dc9813
dskiciak <dskiciak@gmail.com>
2021-05-30 20:30:22 +0200

    feat: add simple footer

---
+++ src/components/Menu/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sass/Footer.sass';
+
+const Footer = () => {
+    return (
+        <div className="menu__footer">
+            Footer
+        </div>
+    );
+};
+
+export default Footer;
